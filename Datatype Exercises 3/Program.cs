c1c2b23add86388c2a123584aa46cf8d0c942674
Onehundra <hermanssonkevin@gmail.com>
2026-01-03 14:41:17 +0100

    Add project files.

---
+++ Datatype Exercises 3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Datatype_Exercises_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Jesus";
+            string lastName = "Kristus";
+            string hobby = "Walking on wine";
+            
+        }
+    }
+}
